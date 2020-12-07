61d36020f3ab90913d1416459afe3699e0b4014b
DanielJamesK <danieljkurz@gmail.com>
2020-12-08 00:44:44 +1000

    Adding HTML markup, styling to card, and embed the IDE

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+const card = document.querySelector('.card__inner')
 const iframe = document.querySelector('iframe')
 const btn = document.getElementById('show')
+const left = document.getElementById('c-l')
+const wholecard = document.getElementById('card')
+
+card.addEventListener('click', function () {
+  card.classList.toggle('is-flipped')
+})
 
 btn.addEventListener('click', function () {
-  if (btn.innerHTML = "Show" && iframe.classList.contains('hidden')) {
+  if (btn.innerHTML = "Try for yourself!" && iframe.classList.contains('hidden')) {
     iframe.classList.toggle('hidden')
-    btn.innerHTML = "hide"
-  } else if (btn.innerHTML = "hide" && !iframe.classList.contains('hidden')) {
+    btn.innerHTML = "Hide"
+  } else if (btn.innerHTML = "Hide" && !iframe.classList.contains('hidden')) {
     iframe.classList.toggle('hidden')
-    btn.innerHTML = 'Show'
+    btn.innerHTML = 'Try for yourself!'
   }
   console.log("clicked")
 })
