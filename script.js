208d95d307ee488ee32c7df0fa7567de3b82acdb
DanielJamesK <danieljkurz@gmail.com>
2020-12-11 22:38:44 +1000

    Added functionality that allows flashcards to be dynamically created with custom questions.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const card = document.querySelector('.card')
+const card = document.querySelector('.card__inner')
 const iframe = document.querySelector('iframe')
 const btn = document.getElementById('show')
 const left = document.getElementById('c-l')
@@ -32,7 +32,7 @@ const addFlashCard = (e) => {
 
   console.warn('added', { questions })
   let pre = document.querySelector('#msg pre')
-  pre.textContent = '\n' + JSON.stringify(questions, '\t', 2)
+  // pre.textContent = '\n' + JSON.stringify(questions, '\t', 2)
 
   localStorage.setItem(`${question.id}`, `${question.description}`)
   userQuestion = localStorage.getItem('MyQuestions')
@@ -44,30 +44,56 @@ const addFlashCard = (e) => {
 document.querySelector('#btn').addEventListener('click', addFlashCard)
 
 function createUserCard(question) {
-  const cardHTML = ` 
-  <div class="card">
-    <div class="card__inner">
-      <div class="card__face card__face--front">
-        <img src="images/html5.png" alt="" class="pp--front">
-        <h3>${question}</h3>
-      </div>
-      <div class="card__face card__face--back">
-      <div class="card__content">
-        <div class="card__header">
-          <h2>Inserting an<br>image</h2>
-        </div>
-          <div class="card__body">
-            <h3>Answer</h3>
-            <p>To insert an image into your HTML markup, use the &lt; img &gt; tags.<br><br>For example <br>&lt; img src="puppy.jpg" &gt; <br><br>Try for yourself in the code editor</p>
-          </div>
-      </div>
-    </div>
-    </div>
-  </div>`
+  // const cardHTML = ` 
+  // <div class="card">
+  //   <div class="card__inner">
+  //     <div class="card__face card__face--front">
+  //       <img src="images/html5.png" alt="" class="pp--front">
+  //       <h3>${question}</h3>
+  //     </div>
+  //     <div class="card__face card__face--back">
+  //     <div class="card__content">
+  //       <div class="card__header">
+  //         <h2>Inserting an<br>image</h2>
+  //       </div>
+  //         <div class="card__body">
+  //           <h3>Answer</h3>
+  //           <p>To insert an image into your HTML markup, use the &lt; img &gt; tags.<br><br>For example <br>&lt; img src="puppy.jpg" &gt; <br><br>Try for yourself in the code editor</p>
+  //         </div>
+  //     </div>
+  //   </div>
+  //   </div>
+  // </div>`
 
-  qContainer.innerHTML = cardHTML
-  cardHTML.addEventListener('click', function () {
-    cardHTML.classList.toggle('is-flipped')
+  // qContainer.innerHTML = cardHTML
+  let div = document.createElement('div');
+  let text = document.createTextNode('Test');
+  let divInner = document.createElement('div')
+  let divInnerFace = document.createElement('div')
+  let divInnerFaceH3 = document.createElement('h3')
+  let divImg = document.createElement('img')
+  div.classList.add('card');
+  divInnerFaceH3.classList.add('card__header')
+  divInner.classList.add('card__inner')
+  divInnerFace.classList.add('card__face')
+  divInnerFace.classList.add('card__face--front')
+  divImg.classList.add('pp-front')
+  divImg.classList.add('pp')
+  divImg.src = "images/html5.png"
+  divInnerFaceH3.textContent = `${question}`
+  divInnerFace.appendChild(divImg)
+  divInnerFace.appendChild(divInnerFaceH3)
+  divInner.appendChild(divInnerFace)
+  divInner.appendChild(text);
+  div.appendChild(divInner);
+  qContainer.appendChild(div)
+
+  div.addEventListener('click', function () {
+    divInnerFace.classList.add('is-flipped')
   })
 }
+
+
+
+
 
